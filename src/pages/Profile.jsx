108403652c9ef932af28e37e0a962812a154f456
Shayanarshi <arshishayan@gmail.com>
2026-09-02 17:13:54 +0530

    Make profile responsive

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,13 +9,13 @@ const Profile = () => {
   return (
    <article className='w-full h-screen flex justify-center items-center' >
               <section className='py-14 px-16 bg-slate-800 text-white rounded-4xl ' >
-                  <h2 className='text-6xl font-extrabold pb-15 text-center '>
+                  <h2 className='text-4xl font-extrabold pb-15 text-center '>
                       Welcome back {loginUserData.username.toUpperCase()} !
                   </h2>
               <article className='w-full h-fit flex gap-6 border p-5 rounded-2xl ' >
                 <img src={user} alt={loginUserData.username} className='w-[30%] rounded-full border' />
                       <section className='w-[60%]  font-bold text-2xl '>
-                          <p>userName : {loginUserData.username} </p>
+                          <p>userame : {loginUserData.username} </p>
                           <p>email : {loginUserData.email} </p>
                       </section>
                   </article>
